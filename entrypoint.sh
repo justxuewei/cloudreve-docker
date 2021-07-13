c0bf2f094e59d87e8aeb190087e356ac5b8438d6
XavierNiu <a@nxw.name>
2021-07-13 12:10:06 +0800

    fix bug(#44)
    fix bug(#44)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
 # database
-CLOUDREVE_DEFAULT_DB=cloudreve.db
-CLOUDREVE_DB=db/cloudreve.db
+CLOUDREVE_DEFAULT_DB=/cloudreve/cloudreve.db
+CLOUDREVE_DB=/cloudreve/db/cloudreve.db
 # conf
-CLOUDREVE_DEFAULT_CONF=conf.ini
-CLOUDREVE_CONF=config/conf.ini
+CLOUDREVE_DEFAULT_CONF=/cloudreve/conf.ini
+CLOUDREVE_CONF=/cloudreve/config/conf.ini
 # init log
-CLOUDREVE_INIT_LOG=init.log
+CLOUDREVE_INIT_LOG=/cloudreve/init.log
 
 if [ ! -e $CLOUDREVE_DB ]; then
-    if [ -e CLOUDREVE_DEFAULT_CONF ]; then
-        echo "Found conf at `$CLOUDREVE_DEFAULT_CONF`, it has been moved to `$CLOUDREVE_CONF`"
+    if [ -e $CLOUDREVE_DEFAULT_CONF ]; then
+        echo "Found config at `$CLOUDREVE_DEFAULT_CONF`, it has been moved to `$CLOUDREVE_CONF`"
         mv $CLOUDREVE_DEFAULT_CONF $CLOUDREVE_CONF
     fi
-    if [ ! e $CLOUDREVE_DEFAULT_DB ]; then
-        nohup cloudreve -c $CLOUDREVE_CONF > $CLOUDREVE_INIT_LOG 2>&1 &
-        echo "Waiting Cloudreve initializing..."
-        sleep 5
-        kill -9 $(ps -ef | grep cloudreve-main | grep -v grep | awk {'print $2'})
-        cat $CLOUDREVE_INIT_LOG
-        rm -f $CLOUDREVE_INIT_LOG
-    fi
-    mv $CLOUDREVE_DEFAULT_DB $CLOUDREVE_DB
     echo -e "[Database]\nDBFile=$CLOUDREVE_DB" >> $CLOUDREVE_CONF
+
+    if [ -e $CLOUDREVE_DEFAULT_DB ]; then
+        echo "Move the database file from `$CLOUDREVE_DEFAULT_DB` to `$CLOUDREVE_DB`"
+        mv $CLOUDREVE_DEFAULT_DB $CLOUDREVE_DB
+    fi
 fi
 
 cloudreve -c $CLOUDREVE_CONF
-
